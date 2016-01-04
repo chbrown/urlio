4dbbaf6975fa03854a2a3ab4a35106dd417b8149
Christopher Brown <io@henrian.com>
2016-01-03 21:36:12 -0600

    Treat specific route objects as their own identifier (no more id); incorporate (optional) method matching

--- index.ts
+++ index.ts
@@ -1,6 +1,18 @@
+/**
+Half-hearted (two-hearted?) shim for Object.assign.
+*/
+function assign<T, U>(target: T, source: U): T & U {
+  for (var key in source) {
+    target[key] = source[key];
+  }
+  return <any>target;
+}
+
 export interface Route {
-  id: string;
-  pattern: string;
+  /** The pattern to match against requested urls. */
+  url: string;
+  /** If provided on the route and in the parse method, only routes with matches are returned. */
+  method?: string;
 }
 
 export interface Params {
@@ -15,9 +27,9 @@ with two fields, e.g.,
       regExp: /\/users\/([^/?#]+)\/([^/?#]+)/,
     }
 */
-export function compilePattern(pattern: string) {
+function compilePattern(url: string): {paramNames: string[], regExp: RegExp} {
   let paramNames: string[] = [];
-  let source = pattern
+  let pattern = url
     // escape forward slashes (/)
     .replace(/\//g, '\\/')
     // replace :varName segments with a group that matches everything but /, ?, and #
@@ -30,49 +42,40 @@ export function compilePattern(pattern: string) {
       paramNames.push('splat');
       return '(.*?)';
     });
-  return {paramNames, regExp: new RegExp(source)};
-}
-
-function compileRoute({id, pattern}: Route) {
-  let {paramNames, regExp} = compilePattern(pattern);
-  return {id, pattern, paramNames, regExp};
+  return {paramNames, regExp: new RegExp(pattern)}; // '^' + pattern + '$'
 }
 
 /**
 Given a path, find the matching Route and extract the values of its params for that path.
+Returns undefined if no routes match.
 */
-export function parse(routes: Route[], path: string) {
+export function parse<T extends Route>(routes: T[], {url, method}: {url: string, method?: string}): T & {params: Params} {
   let params: Params = {};
   // compile the routes
-  const compiledRoutes = routes.map(compileRoute);
+  const compiledRoutes = routes.map(route => {
+    return assign(compilePattern(route.url), route);
+  });
   // find the matching route
-  const matchingRoute = compiledRoutes.find(route => route.regExp.test(path));
+  const compareMethod = method !== undefined;
+  const matchingRoute = compiledRoutes.find(route => route.regExp.test(url) && (compareMethod ? method === route.method : true));
   if (matchingRoute === undefined) {
-    // console.error('Cound not match route for url:', url);
-    return {id: null, params};
+    return undefined;
   }
   // the regExp.test worked, so match should too
   // TODO: avoid executing the matching regex twice (is test any cheaper than match?)
-  const match = path.match(matchingRoute.regExp);
+  const match = url.match(matchingRoute.regExp);
   matchingRoute.paramNames.map((paramName, i) => {
     params[paramName] = match[i + 1];
   });
-  return {id: matchingRoute.id, params};
+  return assign({params}, matchingRoute);
 }
 
 /**
-Given an id and params, return the matching url.
-
-If there is no route for the given `id`, returns ''.
+Given a route and params (like those that would be extracted from it),
+return the matching url.
 */
-export function stringify(routes: Route[], id: string, params: Params = {}) {
-  let compiledRoutes = routes.map(compileRoute);
-  let matchingRoute = compiledRoutes.find(route => route.id === id);
-  if (matchingRoute === undefined) {
-    // console.error('Could not find route for state:', state);
-    return '';
-  }
-  return matchingRoute.pattern
+export function stringify(route: Route, params: Params = {}) {
+  return route.url
     // replace :varName segments with the named value in params
     .replace(/:(\w+)/g, (match, group1) => params[group1])
     // replace * with the value of the param named 'splat' (where undefined evaluates to '')
